711757d353e37352de69779133bc983498168c82
Choi <wldnd12223@gmail.com>
2024-05-02 19:11:13 +0900

    update35

--- js/top4.js
+++ js/top4.js
@@ -9,9 +9,9 @@ top4.innerHTML = `
 
   <div class="owl-carousel clients-carousel">
    
-    <!-- <div><a href="kl-c.html"><div style="position:relative;"><img src="./img/other1.png" class="wide br50" alt="Thumbnail"/></div>
+    <div><a href="kl-c.html"><div style="position:relative;"><img src="./img/other1.png" class="wide br50" alt="Thumbnail"/></div>
         <div style="position:absolute;top:5px;left:35px;color:#fff;"><span></span></div>
-        <div style="position:absolute;top:18%;right:0;left:0;color:#fff;"><span>농산물품질관리사</span><br><br><p>[대기업, 공기업 스펙]<br>[국가공인자격증]<br><span class="blink">[초봉 최대 6000 만원]</span></p></div></a></div> -->
+        <div style="position:absolute;top:18%;right:0;left:0;color:#fff;"><span>농산물품질관리사</span><br><br><p>[대기업, 공기업 스펙]<br>[국가공인자격증]<br><span class="blink">[초봉 최대 6000 만원]</span></p></div></a></div>
 
 
     <!-- <div><a href="kl-e.html"><div style="position:relative;"><img src="./img/other7.png" class="wide br50" alt="Thumbnail"/></div>
@@ -46,26 +46,26 @@ top4.innerHTML = `
   <div class="owl-carousel clients-carousel">
 
     <div id="client1"><a href="./kl-a.html"><div style="position:relative;"><img src="./img/other6.png" class="wide br40" alt="Thumbnail"/></div>
-        <div style="position:absolute;top:5px;left:35px;color:#fff;"><span>TOP 1 )</span></div>
+        <div style="position:absolute;top:5px;left:45px;color:#fff;"><span>TOP 1 )</span></div>
         <div style="position:absolute;top:18%;right:0;left:0;color:#fff;"><span>손해평가사</span><br><br><p>[대기업, 공기업 스펙]<br>[국가공인자격증]<br><span class="blink">[연봉 1억 가능]<br>[정년 없이 전문직 대우]</span></p></div></a></div>
 
         <div id="client2"><a href="kl-b.html"><div style="position:relative;"><img src="./img/other2.png" class="wide br50" alt="Thumbnail"/></div>
-        <div style="position:absolute;top:5px;left:35px;color:#fff;"><span>TOP 2 )</span></div>
+        <div style="position:absolute;top:5px;left:45px;color:#fff;"><span>TOP 2 )</span></div>
         <div style="position:absolute;top:18%;right:0;left:0;color:#fff;"><span>도로교통사고감정사</span><br><br><p>[경찰 공무원 가산점]<br>[국가공인자격증]<br><span class="blink">[높은 취업률]</span></p></div></a></div>
    
-   <!-- <div id="client2"><a href="./kl-c.html"><div style="position:relative;"><img src="./img/other1.png" class="wide br50" alt="Thumbnail"/></div>
-        <div style="position:absolute;top:5px;left:35px;color:#fff;"><span>TOP 2 )</span></div>
-        <div style="position:absolute;top:18%;right:0;left:0;color:#fff;"><span>농산물품질관리사</span><br><br><p>[대기업, 공기업 스펙]<br>[국가공인자격증]<br><span class="blink">[초봉 최대 6000 만원]<br>[인력 부족, 높은 취업률]</span></p></div></a></div> -->
+        <div id="client2"><a href="./kl-c.html"><div style="position:relative;"><img src="./img/other1.png" class="wide br50" alt="Thumbnail"/></div>
+        <div style="position:absolute;top:5px;left:45px;color:#fff;"><span>TOP 2 )</span></div>
+        <div style="position:absolute;top:18%;right:0;left:0;color:#fff;"><span>농산물품질관리사</span><br><br><p>[대기업, 공기업 스펙]<br>[국가공인자격증]<br><span class="blink">[초봉 최대 6000 만원]<br>[인력 부족, 높은 취업률]</span></p></div></a></div>
 
 
    <!-- <div id="client3"><a href="./kl-e.html"><div style="position:relative;"><img src="./img/other7.png" class="wide br50" alt="Thumbnail"/></div>
-        <div style="position:absolute;top:5px;left:35px;color:#fff;"><span>TOP 3 )</span></div>
+        <div style="position:absolute;top:5px;left:45px;color:#fff;"><span>TOP 3 )</span></div>
         <div style="position:absolute;top:18%;right:0;left:0;color:#fff;"><span>맞춤형화장품조제관리사</span><br><br><p>[국가공인자격증]<br><span class="blink">[화장품 대기업 우선채용]</span><br><span>[초봉 3000 이상]</span></p></div></a></div> -->
 
 
     
     <div id="client4"><a href="./bs.html"><div style="position:relative;"><img src="./img/other4.png" class="wide br50" alt="Thumbnail"/></div>
-        <div style="position:absolute;top:5px;left:35px;color:#fff;"><span>TOP 3 )</span></div>
+        <div style="position:absolute;top:5px;left:45px;color:#fff;"><span>TOP 3 )</span></div>
         <div style="position:absolute;top:18%;right:0;left:0;color:#fff;"><span>베이비시터</span><br><br><p>[시급 6만원 가능]<br>[학업 병행 가능]<br>[편한 조정 시간]</p></div></a></div>
 
         <div id="client7"><a href="kl-f.html"><div style="position:relative;"><img src="./img/sports1.png" class="wide br40" alt="Thumbnail"/></div>
